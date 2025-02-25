aa0da6d6c75e5ed07e46fc9a239aeca5c1dc54f4
LarsVDD05 <vandendaelelars@gmail.com>
2025-02-25 12:34:07 +0100

    finish home

---
+++ assets/data/data/camps.js
@@ -0,0 +1,77 @@
+const camps = [
+    {
+        "week": "WEEK 1",
+        "periode": "01-04 JULI (*)",
+        "grommetsSurfcamp": "€185 / €165",
+        "juniorSurfcamp": "€185 / €165",
+        "grommetsMorningCamp": "€100 / €80",
+        "boardAndBikeCamp": "€185 / €165"
+    },
+    {
+        "week": "WEEK 2",
+        "periode": "07-11 JULI",
+        "grommetsSurfcamp": "€230 / €210",
+        "juniorSurfcamp": "€230 / €210",
+        "grommetsMorningCamp": "€120 / €100",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 3",
+        "periode": "14-18 JULI",
+        "grommetsSurfcamp": "€210 / €190",
+        "juniorSurfcamp": "€210 / €190",
+        "grommetsMorningCamp": "€120 / €100",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 4",
+        "periode": "21-25 JULI",
+        "grommetsSurfcamp": "€210 / €190",
+        "juniorSurfcamp": "€210 / €190",
+        "grommetsMorningCamp": "-",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 5",
+        "periode": "28-01 AUG",
+        "grommetsSurfcamp": "€210 / €190",
+        "juniorSurfcamp": "€210 / €190",
+        "grommetsMorningCamp": "-",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 6",
+        "periode": "04-08 AUG",
+        "grommetsSurfcamp": "€210 / €190",
+        "juniorSurfcamp": "€210 / €190",
+        "grommetsMorningCamp": "-",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 7",
+        "periode": "11-15 AUG",
+        "grommetsSurfcamp": "€210 / €190",
+        "juniorSurfcamp": "€210 / €190",
+        "grommetsMorningCamp": "€120 / €100",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 8",
+        "periode": "18-22 AUG",
+        "grommetsSurfcamp": "€230 / €210",
+        "juniorSurfcamp": "€230 / €210",
+        "grommetsMorningCamp": "€120 / €100",
+        "boardAndBikeCamp": "-"
+    },
+    {
+        "week": "WEEK 9",
+        "periode": "25-29 AUG",
+        "grommetsSurfcamp": "€230 / €210",
+        "juniorSurfcamp": "€230 / €210",
+        "grommetsMorningCamp": "€120 / €100",
+        "boardAndBikeCamp": "€230 / €210"
+    }
+];
+
+module.exports = camps;
+
---
+++ assets/data/index.js
@@ -0,0 +1,9 @@
+const camps = require("./data/camps");
+
+function getCamps() {
+    return camps;
+}
+
+module.exports = {
+     getCamps
+}
